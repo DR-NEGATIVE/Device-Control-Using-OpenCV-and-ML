71cf649362fa3d751ccf7095281b210e5e04e807
DR-NEGATIVE <ojhastar01@gmail.com>
2021-02-17 20:16:57 +0530

    intial work

--- opencv/Gesture_Opencv.py
+++ opencv/Gesture_Opencv.py
@@ -1,1 +1,13 @@
-print("hello")
+import cv2
+c=0
+img=cv2.VideoCapture(0)
+while True:
+    rt,frame=img.read()
+    frame=cv2.cvtColor(frame,cv2.COLOR_RGB2BGR) #cv2.COLOR_RGB2BGR working well
+    cv2.imshow("ok",frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        
+        break
+
+img.realese()
+cv2.destroyAllWindows()
